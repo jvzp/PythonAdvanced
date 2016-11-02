db3e4ed80852b5790f8a659ca85cf6beed02e5dc
jvzp <jonas.vanzurpele@altran.com>
2016-11-02 10:02:33 +0100

    Exercises on Strings

---
+++ Strings/Ex1/FileSanityChecker.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python
+
+with open("file.txt", "r") as f:
+    for line_number,line in enumerate(f):
+	temp = line.rstrip('\n')
+        numbers = temp.split(",")
+	for column,x in enumerate(numbers):
+            if not x.isdigit():
+                print "No digit on line: {} in column: {}, character is: {}".format(line_number, column, x)
+
